c41fd97519bb5e45cf9914f443b93066dc94daa2
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-07-03 13:02:15 -0300

    Parte 02

---
+++ _inserir_produto.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+$nro_produto = $_POST['nro'];
+$nome_produro = $_POST['nome'];
+$categotia_produto = $_POST['categoria'];
+$quantidade_produto = $_POST['qtd'];
+$fornecedor_produto = $_POST['fornecedor'];
+
+echo $nro_produto."<br/>"; 
+echo $nome_produro."<br/>";
+echo $categotia_produto."<br/>";
+echo $quantidade_produto."<br/>";
+echo $fornecedor_produto."<br/>";
+
+
+?>
