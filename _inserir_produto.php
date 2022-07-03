c85dc5acb449b2a732bbd8d98b0e7473a04b02e9
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-07-03 13:51:23 -0300

    Parte 03

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -1,18 +1,24 @@
 <?php
 
+include "conexao.php";
+
+$nro = $_POST['nro'];
+$nome = $_POST['nome'];
+$categoria = $_POST['categoria'];
+$qtd = $_POST['qtd'];
+$fornecedor = $_POST['fornecedor'];
+
+// echo $nro_produto."<br/>"; 
+// echo $nome_produro."<br/>";
+// echo $categotia_produto."<br/>";
+// echo $quantidade_produto."<br/>";
+// echo $fornecedor_produto."<br/>";
 
 
-$nro_produto = $_POST['nro'];
-$nome_produro = $_POST['nome'];
-$categotia_produto = $_POST['categoria'];
-$quantidade_produto = $_POST['qtd'];
-$fornecedor_produto = $_POST['fornecedor'];
+$sql = "INSERT INTO `estoque`( `nro`, `nome`, `categoria`, `qtd` ,`fornecedor`) VALUES ($nro,'$nome','$categoria',$qtd,'$fornecedor')";
 
-echo $nro_produto."<br/>"; 
-echo $nome_produro."<br/>";
-echo $categotia_produto."<br/>";
-echo $quantidade_produto."<br/>";
-echo $fornecedor_produto."<br/>";
+// INSERT INTO `estoque`( `nro`, `nome`, `categoria`, `qtd` ,`fornecedor`) VALUES (111,'Tv 50','Periféricos',22,'Fornecedor A')
 
+$inserir = mysqli_query($conexao,$sql);
 
 ?>
